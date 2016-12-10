ac0c6402f6e60623bf47b4a795eca699f081ce39
Joshua <robofrog1235@aol.com>
2016-12-10 16:48:23 -0600

    Add distance function

--- ld37/common/utils/libutils.py
+++ ld37/common/utils/libutils.py
@@ -1,4 +1,13 @@
+import math
+
 def update_image_rect(image, rect):
     image_rect = image.get_rect()
     image_rect.x = rect.x
     image_rect.y = rect.y
+
+def distance_between_rects(rect1, rect2):
+    (r1_center_x, r1_center_y) = rect1.center
+    (r2_center_x, r2_center_y) = rect2.center
+    x_squared = (r1_center_x - r2_center_x)**2
+    y_squared = (r1_center_y - r2_center_y)**2
+    math.sqrt(x_squared + y_squared)
