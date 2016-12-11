28fe6a0a1e5e5d8781854aad4f22d368d3d73b12
Joshua <robofrog1235@aol.com>
2016-12-10 18:18:08 -0600

    Update distance formula to be more standard

--- ld37/common/utils/libutils.py
+++ ld37/common/utils/libutils.py
@@ -8,6 +8,6 @@ def update_image_rect(image, rect):
 def distance_between_rects(rect1, rect2):
     (r1_center_x, r1_center_y) = rect1.center
     (r2_center_x, r2_center_y) = rect2.center
-    x_squared = (r1_center_x - r2_center_x)**2
-    y_squared = (r1_center_y - r2_center_y)**2
-    math.sqrt(x_squared + y_squared)
+    x_squared = (r2_center_x - r1_center_x)**2
+    y_squared = (r2_center_y - r1_center_y)**2
+    return math.sqrt(x_squared + y_squared)
