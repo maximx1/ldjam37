2da828cd5c9865f716798fb713374bcc775be274
Justin Walrath <walrathjaw@gmail.com>
2016-12-11 00:03:38 -0600

    Added small demo to illustrate difference between collidable and displayable.

--- ld37/common/startupactivities.py
+++ ld37/common/startupactivities.py
@@ -6,7 +6,11 @@ def create_starting_entities():
     pc = create_playable_character(1, (0, 0))
     pc.is_current_player_controllable = True
     entities.append(pc)
-    entities.append(create_static_collidable(2, (70, 70)))
+    entities.append(create_static_object(2, (70, 70), True, True))
+    entities.append(create_static_object(2, (105, 70), True, False))
+    entities.append(create_static_object(2, (140, 70), False, True))
+    entities.append(create_static_object(2, (175, 70), False, False))
+    entities.append(create_static_object(2, (210, 70), True, True))
 
     for entity in entities:
         entity.master_entity_list = entities
